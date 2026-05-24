ef45ff13dd8d6474396b065c07166b7493803c7e
Forexbaby <fizafaruki99@gmail.com>
2026-05-24 15:18:42 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,67 @@
+// EURUSD
+
+new TradingView.widget({
+  "width":"100%",
+  "height":300,
+  "symbol":"FX:EURUSD",
+  "interval":"30",
+  "timezone":"Etc/UTC",
+  "theme":"dark",
+  "style":"1",
+  "locale":"en",
+  "toolbar_bg":"#111827",
+  "hide_top_toolbar":true,
+  "hide_legend":true,
+  "container_id":"tv-eurusd"
+});
+
+// BTC
+
+new TradingView.widget({
+  "width":"100%",
+  "height":300,
+  "symbol":"BINANCE:BTCUSDT",
+  "interval":"30",
+  "timezone":"Etc/UTC",
+  "theme":"dark",
+  "style":"1",
+  "locale":"en",
+  "toolbar_bg":"#111827",
+  "hide_top_toolbar":true,
+  "hide_legend":true,
+  "container_id":"tv-btc"
+});
+
+// GOLD
+
+new TradingView.widget({
+  "width":"100%",
+  "height":300,
+  "symbol":"OANDA:XAUUSD",
+  "interval":"30",
+  "timezone":"Etc/UTC",
+  "theme":"dark",
+  "style":"1",
+  "locale":"en",
+  "toolbar_bg":"#111827",
+  "hide_top_toolbar":true,
+  "hide_legend":true,
+  "container_id":"tv-gold"
+});
+
+// NASDAQ
+
+new TradingView.widget({
+  "width":"100%",
+  "height":300,
+  "symbol":"NASDAQ:NDX",
+  "interval":"30",
+  "timezone":"Etc/UTC",
+  "theme":"dark",
+  "style":"1",
+  "locale":"en",
+  "toolbar_bg":"#111827",
+  "hide_top_toolbar":true,
+  "hide_legend":true,
+  "container_id":"tv-nasdaq"
+});
